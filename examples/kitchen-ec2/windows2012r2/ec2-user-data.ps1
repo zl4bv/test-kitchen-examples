ed135928b052984b342f1e263905d436f85e8e64
Ben Vidulich <ben@vidulich.co.nz>
2015-06-03 18:11:37 +1200

    Clean up user data

--- examples/kitchen-ec2/windows2012r2/ec2-user-data.ps1
+++ examples/kitchen-ec2/windows2012r2/ec2-user-data.ps1
@@ -1,19 +1,5 @@
 <powershell>
-write-output "Running User Data Script"
-write-host "(host) Running User Data Script"
- 
-cmd.exe /c net user /add kitchen K1tch3nR0ck5
-cmd.exe /c net localgroup Administrators kitchen /add
- 
 Set-ExecutionPolicy -ExecutionPolicy bypass -Force
- 
-# RDP
-#cmd.exe /c netsh advfirewall firewall add rule name="Open Port 3389" dir=in action=allow protocol=TCP localport=3389
-#cmd.exe /c reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
- 
-# WinRM
-write-output "Setting up WinRM"
-write-host "(host) setting up WinRM"
  
 cmd.exe /c winrm quickconfig -q
 cmd.exe /c winrm quickconfig '-transport:http'
@@ -31,7 +17,6 @@ cmd.exe /c netsh firewall add portopening TCP 5985 "Port 5985"
 cmd.exe /c net stop winrm
 cmd.exe /c sc config winrm start= auto
 cmd.exe /c net start winrm
-cmd.exe /c wmic useraccount where "name='kitchen'" set PasswordExpires=FALSE
  
 </powershell>
 
